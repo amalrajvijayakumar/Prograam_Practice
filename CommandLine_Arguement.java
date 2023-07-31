08c67aef1e1c79eaf551e740dacac3bdf729adc6
Amalraj <amalrajvijayakumar@gmail.com>
2023-07-31 13:29:23 +0530

    CommandLine Arguementq

---
+++ CommandLine_Arguement.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class CommandLine_Arguement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (String t: args){
+			
+			System.out.println(t+ "Command");
+			
+		}
+	}
+
+}
