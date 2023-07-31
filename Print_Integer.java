3df9ecdb201d99c0f8613728ede67e999c58d2a7
Amalraj <amalrajvijayakumar@gmail.com>
2023-07-31 13:30:51 +0530

    Print integer

---
+++ Print_Integer.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Print_Integer {
+	public static void main(String[] args) {
+		int i;
+		
+		for(i=1; i<=5; i++)
+		{
+			System.out.println("Printing " + i);
+		}
+	}
+
+}
