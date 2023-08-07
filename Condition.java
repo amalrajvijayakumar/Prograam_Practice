830235936d78bac563a113f42017c04a8288cc7c
Amalraj <amalrajvijayakumar@gmail.com>
2023-08-07 22:10:22 +0530

    Condition Pratice

---
+++ Condition.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Condition {
+ public static void main(String[] args) {
+
+		boolean learning = true;
+		
+		if (learning) {
+			System.out.println("Java Programmer");
+		}
+		else {
+			System.out.println("What are you doing here");
+		}
+	}
+
+}
