4b7b177dbf93fd7bba9a4544db394ca4e0a7e281
AMALRAJ <127419584+amalrajvijayakumar@users.noreply.github.com>
2023-08-10 17:13:18 +0530

    Update LogicalPractice2.java
    Question was added as a comment on the 6th to 10th line

--- LogicalPractice2.java
+++ LogicalPractice2.java
@@ -3,8 +3,13 @@ package InterviewPractice;
 public class LogicalPractice2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Ques
+		//Test case 1
+		// x= 4, y = 10 expected output = 7
+		//Test Case 2 
+		// x = 10, y =14 expected output = 11
 		int a[]  = {4,3,7,11,50,1};
+		
 		int x=10; 
 		int y=14;
 	
