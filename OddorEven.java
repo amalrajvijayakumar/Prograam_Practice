d9009dcf95291957b9960a8b40323bece6510d34
Amalraj <amalrajvijayakumar@gmail.com>
2023-08-19 10:53:18 +0530

    Odd or Even

---
+++ OddorEven.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class OddorEven {
+	public static void main(String[] args) {
+		int x ;
+		System.out.println("Enter the Number to check odd or even");
+		
+		Scanner in = new Scanner (System.in);
+		x = in.nextInt();
+		
+		if (x %2 == 0)
+		System.out.println("You entered Even Number");
+		else
+			System.out.println("You entered Odd Number");
+	}
+}
