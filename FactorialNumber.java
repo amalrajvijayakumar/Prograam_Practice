f45bb317fef0d6497bd1f138e486987b8c80495a
AMALRAJ <127419584+amalrajvijayakumar@users.noreply.github.com>
2023-12-04 14:26:15 +0530

    Update FactorialNumber.java
    for understand

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -7,20 +7,17 @@ public class FactorialNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n, c, fact =1;
-		 
 		System.out.println(" Enter the Number to calculate it's factorial");
-		
 		Scanner sc =  new Scanner(System.in);
 		
 		n = sc.nextInt();
 	
-		if(n <0 )
+		if(n <0 )// to avoide negative numbers like -5
 			System.out.println("Number should be non-negative");
 		else
 		{
 			for(c=1; c<=n; c++)
 				fact = fact * c;
-		
 		System.out.println("Factorial of " + n + " is = " + fact);
 		}
 	}
